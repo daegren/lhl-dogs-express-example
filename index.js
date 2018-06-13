03c59a2c88dbb4ad174fd9f00a0fa2bd3270ef77
David Mills <dave@1morepx.com>
2018-06-13 12:01:25 -0400

    adding comments

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const uuid = require("uuid/v4");
 
 const app = express();
 
+/**
+ * An array which contains the data for the application
+ */
 let dogsDb = [
   {
     id: "baa22af1-6054-41e6-b1f4-135fa67820d0",
@@ -18,22 +21,37 @@ let dogsDb = [
   }
 ];
 
+/**
+ * Find a dog from the database by ID.
+ *
+ * @param {uuid} id A `UUID` which identifies the dog to find.
+ * @returns { (Object | undefined) } The dog that was found in the database
+ */
 const findDog = id => {
   const dog = dogsDb.filter(dog => dog.id === id)[0];
   return dog;
 };
 
+// Use `ejs` as our view engine
 app.set("view engine", "ejs");
+// Use `body-parser` to parse request bodies.
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/**
+ * A helper to render out a 404 response.
+ *
+ * @param {Express.Response} res The response object.
+ */
 const render404 = res => {
   res.status(404).render("404");
 };
 
+// GET /dogs/new - Create Form
 app.get("/dogs/new", (req, res) => {
   res.render("dogs/new");
 });
 
+// POST /dogs - Create Action
 app.post("/dogs", (req, res) => {
   const newDog = {
     id: uuid(),
@@ -46,6 +64,7 @@ app.post("/dogs", (req, res) => {
   res.redirect(`/dogs/${newDog.id}`);
 });
 
+// GET /dogs/:id/edit - Update Form
 app.get("/dogs/:id/edit", (req, res) => {
   const dog = findDog(req.params.id);
   if (dog) {
@@ -55,6 +74,7 @@ app.get("/dogs/:id/edit", (req, res) => {
   }
 });
 
+// POST /dogs/:id - Update action
 app.post("/dogs/:id", (req, res) => {
   const dog = findDog(req.params.id);
   if (dog) {
@@ -66,11 +86,12 @@ app.post("/dogs/:id", (req, res) => {
   }
 });
 
-// app.get('/dogs', function(req, res) {});
+// GET /dogs - Index for dogs
 app.get("/dogs", (req, res) => {
   res.render("dogs/index", { dogs: dogsDb });
 });
 
+// GET /dogs/:id - Show for dogs
 app.get("/dogs/:id", (req, res) => {
   const dog = findDog(req.params.id);
   if (dog) {
@@ -80,6 +101,7 @@ app.get("/dogs/:id", (req, res) => {
   }
 });
 
+// POST /dogs/:id/delete - Delete a dog
 app.post("/dogs/:id/delete", (req, res) => {
   const foundDog = findDog(req.params.id);
   if (foundDog) {
@@ -90,6 +112,7 @@ app.post("/dogs/:id/delete", (req, res) => {
   }
 });
 
+// Start the app, listening on `PORT`
 app.listen(PORT, () => {
   console.log(`App is running @ http://localhost:${PORT}`);
 });
